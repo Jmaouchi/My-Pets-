acc6cc5584ff1ba888f7c0381ee61b637c35f258
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2023-07-20 23:30:32 -0500

    added some html and css, and upadted folder structures

--- server.js
+++ server.js
@@ -9,5 +9,9 @@ const PORT = process.env.PORT || 3002;
 // middlewares 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// this will help us access files from other files, like css and js from html
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.listen(PORT, () => console.log('Now listening on port ' + PORT));
