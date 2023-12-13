e1cc932297ff10191b6200aa622cdcefd25fc931
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2023-12-13 00:48:47 -0600

    added a submit form and funcionality to it

--- public/JavaScript/showAlert.js
+++ public/JavaScript/showAlert.js
@@ -1,9 +1,20 @@
 const showAlertbutton = document.querySelector('.showAlert')
 const showAlertMessage = document.querySelector('.review-alert')
+const showReviewForm = document.querySelector('.review-submit');
+const continueReview = document.querySelector('.ok');
 
 
-function showTheAlert(){
+
+showTheAlert= () => {
   showAlertMessage.style.display = 'block'
 }
 
-showAlertbutton.addEventListener('click', showTheAlert)
+
+showReviwFormForClient = () => {
+  showAlertMessage.style.display = 'none'
+  showReviewForm.style.display = 'block'
+}
+
+
+showAlertbutton.addEventListener('click', showTheAlert);
+continueReview.addEventListener('click', showReviwFormForClient)
